8bddeacfb6053208c26db57bba61aaa69821d757
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2020-06-12 23:05:23 -0600

    Naming format update

--- Releases/BuildReleases.ps1
+++ Releases/BuildReleases.ps1
@@ -5,15 +5,12 @@ $platforms = @("linux-arm", "linux-x64", "osx-x64", "win-arm", "win-x64", "win-x
 foreach ($platform in $platforms) {
 
     $oldPath = "$platform/latest"
-    $newPath = "$platform/$version-latest"
 
     if (Test-Path $oldPath) {
-        "Renaming '$oldPath' to '$newPath'"
-        Move-Item $oldPath $newPath
-        $archivePath = "$newPath.zip"
-        "Compressing $newPath to $archivePath"
-        Compress-Archive -Path $newPath -DestinationPath $archivePath -CompressionLevel "Optimal"
-        "Cleaning up $newPath"
-        Remove-Item $newPath -Recurse
+        $archivePath = "$platform/$platform-$version.zip"
+        "Compressing $oldPath to $archivePath"
+        Compress-Archive -Path $oldPath -DestinationPath $archivePath -CompressionLevel "Optimal"
+        "Cleaning up $oldPath"
+        Remove-Item $oldPath -Recurse
     }
 }
