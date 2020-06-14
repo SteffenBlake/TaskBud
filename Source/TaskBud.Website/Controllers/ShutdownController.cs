4e1085ec6e90a9a60bec255de0f21fdfe87f045a
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2020-06-14 17:12:28 -0600

    Added Admin controlled remote shutdown endpoint

---
+++ Source/TaskBud.Website/Controllers/ShutdownController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace TaskBud.Website.Controllers
+{
+    [Route("shutdown")]
+    [Authorize(Roles = "Administrator")]
+    public class ShutdownController : Controller
+    {
+        private IHostApplicationLifetime AppLifetime { get; }
+
+        public ShutdownController(IHostApplicationLifetime appLifetime)
+        {
+            AppLifetime = appLifetime ?? throw new ArgumentNullException(nameof(appLifetime));
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            AppLifetime.StopApplication();
+            return new EmptyResult();
+        }
+    }
+}
