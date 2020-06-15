5dadf30a9895678f2b393b6fe76801c031d1250e
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2020-06-14 19:11:37 -0600

    v1.0.2 Release

--- Releases/BuildReleases.ps1
+++ Releases/BuildReleases.ps1
@@ -1,4 +1,4 @@
-$version = "1.0.1"
+$version = "1.0.2"
 
 $platforms = @("linux-arm", "linux-x64", "osx-x64", "win-arm", "win-x64", "win-x86" )
 
@@ -8,6 +8,11 @@ foreach ($platform in $platforms) {
 
     if (Test-Path $oldPath) {
         $archivePath = "$platform/$platform-$version.zip"
+        if (Test-Path $archivePath) {
+            "$Test-Path already exists. Ending script"
+            exit
+        }
+
         "Compressing $oldPath to $archivePath"
         Compress-Archive -Path $oldPath -DestinationPath $archivePath -CompressionLevel "Optimal"
         "Cleaning up $oldPath"
